12359d705f4ad469070252375ce4dc5e2ac3703f
Devang Tomar <devangtomar123@gmail.com>
2023-06-22 23:17:00 +0530

    Fixing react router!

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+// Import your page components
+import About from './app/About/About';
+import Home from './app/Home/Home';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit
-                    <code>src/App.tsx</code>
-                    and save to reload.
-                </p>
-            </header>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/"
+                    element={<Home/>}/>
+                <Route path="/about"
+                    element={<About/>}/>
+            </Routes>
+        </Router>
     );
 }
 
