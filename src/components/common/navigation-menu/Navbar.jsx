afb338a8124501af106ae0dc7b2479dfec730f13
Devang Tomar <devangtomar123@gmail.com>
2023-06-28 11:00:37 +0530

    Making navbar translucent

--- src/components/common/navigation-menu/Navbar.jsx
+++ src/components/common/navigation-menu/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <>
-            <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <nav class="opacity-50 bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" class="flex items-center">
                         <img src="https://avatars.githubusercontent.com/u/32426600?v=4" class="mr-3 w-10 rounded-full" alt="Flowbite Logo" />
