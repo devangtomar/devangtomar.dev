8eb28f1b94c7adc01fb84fc5b85ac92e7137af7e
Devang Tomar <devangtomar123@gmail.com>
2023-07-06 00:50:47 +0530

    Adding footer

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/common/navigation-menu/Navbar"
+import Footer from "../../components/common/Footer/Footer"
 
 export default function Home() {
     return (
         <>
         <Navbar></Navbar>
+        <Footer></Footer>
         </>
     )
 }
