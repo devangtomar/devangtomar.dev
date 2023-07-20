155120b8063cbd8a65b6c57b4e10cb3bf70e074d
Devang Tomar <devangtomar123@gmail.com>
2023-07-20 23:09:54 +0530

    Fixing font size and width

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,14 +3,14 @@ import Footer from "../../components/common/footer/Footer"
 
 export default function Home() {
     return (
-        <>
+        <div>
             <Navbar></Navbar>
             <div className="relative max-w-screen-md flex-wrap mx-auto p-4">
-                <h1 className="max-w-md">Hey, I'm Devang!</h1>
-                <h2 className="max-w-md">Welcome to my digital garden. 🌱</h2>
+                <h1 className="text-4xl max-w-md font-bold">Hey, I'm Devang!</h1>
+                <h2 className="text-xl max-w-md mt-4">Welcome to my digital garden. 🌱</h2>
                 <p className="mt-10 max-w-md">I'm a software developer in Bangalore. I make open-source projects and write about code, design, and life. I like coding, reading, and gaming.</p>
             </div>
             <Footer></Footer>
-        </>
+        </div>
     )
 }
