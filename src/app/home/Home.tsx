6fc6a9851e344a5b5429a701b25f51c05efbd8df
Devang Tomar <devangtomar123@gmail.com>
2023-07-25 23:16:55 +0530

    Adding emojis to the home-page

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className="relative max-w-screen-md flex-wrap mx-auto p-4 pt-24">
                 <h1 className="text-4xl max-w-md font-extrabold">Hey, I'm Devang!</h1>
                 <h2 className="text-xl max-w-md mt-4 font-semibold">Welcome to my digital garden. 🌱</h2>
-                <p className="mt-4 max-w-md">I'm a software developer in Bangalore. I make open-source projects and write about code, design, and life. I like coding, reading, and gaming.</p>
+                <p className="mt-4 max-w-md break-words">I'm a software developer 👨‍💻 currently based in Bangalore 🌆. I make open-source projects 🌟 and write about code, design, and life 📝. I enjoy coding 🖥️, reading 📚, and gaming 🎮.</p>
             </div>
             <Footer></Footer>
         </div>
