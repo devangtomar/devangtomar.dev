b4963685df0c80751b2a7d06553a55326ed550a0
Devang Tomar <devangtomar123@gmail.com>
2023-07-27 00:24:26 +0530

    Added sticky navbar and removed avatar

--- src/components/common/navigation-menu/Navbar.jsx
+++ src/components/common/navigation-menu/Navbar.jsx
@@ -1,17 +1,13 @@
 export default function Navbar() {
     return (
         <>
-            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" class="flex items-center">
                         <img src="https://avatars.githubusercontent.com/u/32426600?v=4" class="mr-3 w-10 rounded-full" alt="Flowbite Logo" />
                         <span class="self-center text-xl font-bold whitespace-nowrap dark:text-white">Devang Tomar</span>
                     </a>
                     <div class="flex items-center md:order-2">
-                        <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="w-8 h-8 rounded-full" src="" alt=""></img>
-                        </button>
                         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                             <div class="px-4 py-3">
                                 <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
