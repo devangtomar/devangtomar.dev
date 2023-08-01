2f9168cbcc8a38d785ec24159f7c4d9031e29e4c
Devang Tomar <devangtomar123@gmail.com>
2023-08-01 23:47:15 +0530

    Fixing dark mode colors

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
     return (
         <>
-            <footer className="opacity-50 bg-white dark:bg-gray-900 fixed w-full z-20 bottom-0 left-0  ">
+            <footer className="opacity-50 bg-white dark:bg-gray-800 fixed w-full z-20 bottom-0 left-0  ">
                 <div className="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <div>
                         <span className="font-semibold dark:text-white">Made with ❤️ by Devang</span>
                     </div>
                     <div className="items-center justify-between md:flex md:w-auto" id="navbar-user">
-                        <ul className="flex font-medium p-4 space-x-4  md:bg-white dark:bg-gray-900 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex font-medium p-4 space-x-4  md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
                             <li>
                                 <img className='w-8' src="https://img.icons8.com/doodle/48/twitter-circled.png" alt="twitter-circled"/>
                             </li>
--- src/components/common/navigation-menu/Navbar.jsx
+++ src/components/common/navigation-menu/Navbar.jsx
@@ -3,7 +3,7 @@ import { Dark } from "../theme/Dark"
 export default function Navbar() {
     return (
         <>
-            <nav class="opacity-50 bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+            <nav class="opacity-50 bg-white dark:bg-gray-800 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" class="flex items-center">
                         <img src="https://avatars.githubusercontent.com/u/32426600?v=4" class="mr-3 w-10 rounded-full" alt="Flowbite Logo" />
@@ -71,7 +71,7 @@ export default function Navbar() {
                         </div>
                     </div>
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-700 md:dark:bg-gray-800 dark:border-gray-700">
                             <li>
                                 <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
                             </li>
