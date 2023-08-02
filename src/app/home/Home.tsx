c2f538e72fa3aa5b2f8385c047980e5f0edf2f1b
Devang Tomar <devangtomar123@gmail.com>
2023-08-02 23:50:54 +0530

    Adding dark mode to app.tsx

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -3,10 +3,10 @@ import Footer from "../../components/common/footer/Footer"
 
 export default function Home() {
     return (
-        <div>
+        <div className="">
             <Navbar></Navbar>
-            <div className="max-w-screen-md flex-row flex items-center justify-between mx-auto pt-10">
-                <div className=" flex-wrap mx-auto p-4">
+            <div className="max-w-screen-md flex-row flex items-center justify-between mx-auto pt-10 ">
+                <div className=" flex-wrap mx-auto p-4 dark:text-white">
                     <h1 className="text-4xl max-w-md font-extrabold">Hey, I'm Devang!</h1>
                     <h2 className="text-xl max-w-md mt-4 font-semibold">Welcome to my digital garden. 🌱</h2>
                     <p className="mt-4 max-w-md break-words">I'm a software developer 👨‍💻 currently based in Bangalore 🌆. I make open-source projects 🌟 and write about code, design, and life 📝. I enjoy coding 🖥️, reading 📚, and gaming 🎮.</p>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')as HTMLElement);
+root.render (
+    <React.StrictMode>
+        <div className='bg-white dark:bg-gray-700'>
+            <App/>
+        </div>
+    </React.StrictMode>
 );
